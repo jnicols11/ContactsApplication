9afd5bb1bc80223676357acc08fe15c2d4725da0
jnicols11 <46580604+jnicols11@users.noreply.github.com>
2019-04-28 22:44:32 -0700

    Version 1.2 (Loading file still doesnt work.

--- app/src/main/java/com/example/nicol/contactsapplication/editPerson.java
+++ app/src/main/java/com/example/nicol/contactsapplication/editPerson.java
@@ -1,6 +1,7 @@
 package com.example.nicol.contactsapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -85,6 +86,74 @@ public class editPerson extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        btn_email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String [] addresses = new String [1];
+                addresses[0] = et_email.getText().toString();
+
+                composeEmail(addresses, "New Email.");
+            }
+        });
+
+        btn_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialPhoneNumber(et_phone.getText().toString());
+            }
+        });
+
+        btn_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                composeMmsMessage(et_phone.getText().toString());
+            }
+        });
+
+        btn_gps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mapsQuery = "geo:0,0?q= " + et_address.getText().toString();
+                Uri mapuri = Uri.parse(mapsQuery);
+                showMap(mapuri);
+            }
+        });
+    }
+
+    public void composeEmail(String[] addresses, String subject){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+    }
+
+    public void dialPhoneNumber(String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void composeMmsMessage(String message) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("smsto:" + message));  // This ensures only SMS apps respond
+        intent.putExtra("sms_body", "");
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void showMap(Uri geoLocation) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
 
